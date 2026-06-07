3040f512cd4d870870a6aa6a9a66a30f063c0998
PamelaPainepilDev <spb.pame01@gmail.com>
2026-06-06 21:12:29 -0400

    feat: agregar diseño oscuro celeste y animaciones

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use("/api", apiRouter);
